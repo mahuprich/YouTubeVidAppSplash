5bbd10b206be92c8e59db31c95133bcaf9e4f861
mahuprich <wve943@my.utsa.edu>
2020-09-13 05:38:26 -0500

    Initial Commit

---
+++ YouTubeVidAppSplash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeVidAppSplash
+//
+//  Created by Maximilian Huprich on 9/13/20.
+//  Copyright © 2020 teamhupma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

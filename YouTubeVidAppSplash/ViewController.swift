120d37f0f30f8d8bb31c4e941c995b8ad47221b7
mahuprich <wve943@my.utsa.edu>
2020-09-13 07:39:07 -0500

    used webKit for Xcode 11 added if func at the end to make sure url is good

--- YouTubeVidAppSplash/ViewController.swift
+++ YouTubeVidAppSplash/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var ytWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        getVideo(videoCode: "C0Z6tJdeQ_E")
+        
     }
 
-
+    func getVideo(videoCode:String) {
+        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
+        ytWebView.load(URLRequest(url: url!))
+        
+        if url == nil {
+            print("Video not Found")
+        }
+        
+    }
+    
 }
 
